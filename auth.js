ce583ab6ef835a27be837c6b81abdbe3662e90be
dharmik2212 <dharmikpatelpatel3156@gmail.com>
2025-08-26 14:32:17 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const oAuth2Client = new OAuth2Client(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'postmessage'
+    "http://localhost:3000/auth/google/callback"
 );
 
 // Google authentication callback
@@ -76,3 +76,4 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
+
